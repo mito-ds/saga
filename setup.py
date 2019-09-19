97265ed667e8c149bdabba9808731e3ab9b2338b
naterush <naterush1997@gmail.com>
2019-09-19 00:15:03 -0400

    add missing package init files, update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="saga-vcs",
-    version="0.0.2",
+    version="0.0.5",
     author="saga",
     author_email="narush@wharton.upenn.edu.com",
     description="saga is a set of tools for version control",
