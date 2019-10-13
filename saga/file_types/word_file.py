2eeaea98b0b6179616082b6d7ea5ff5c30288da0
naterush <naterush1997@gmail.com>
2019-10-13 19:35:01 -0400

    comments

--- saga/file_types/word_file.py
+++ saga/file_types/word_file.py
@@ -2,11 +2,9 @@ from saga.base_file.File import File
 from docx import Document
 from os import remove
 
-# We model a word document as a list of paragraphs, for now
-
-
 def parse_word_file(file_id, file_name, file_path):
     document = Document(file_path)
+    # for now, we model a word document as a list of paragraphs
     file_contents = [p.text for p in document.paragraphs]
     return File(file_id, "word", file_path, file_name, file_contents)
 
