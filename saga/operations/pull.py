06ec9dd12823c45c6c1fb1ba4827b00623d0475e
naterush <naterush1997@gmail.com>
2020-01-29 21:56:49 -0500

    pull the right project

--- saga/operations/pull.py
+++ saga/operations/pull.py
@@ -2,6 +2,6 @@ from saga.Repository import Repository
 from saga.operations.get_remote import get_remote
 
 def pull(repository: Repository):
-    url = f"{repository.remote_repository}/cli/pull"
+    url = f"{repository.remote_repository}/cli/pull/{repository.base_directory.name}"
     get_remote(url)
     print(f"Got remote from {url}")
